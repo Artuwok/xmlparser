d2a995cc7073c52e14bbbaf1b07f38392a20c198
Artuwok1986 <artem.vlasenko@gmail.com>
2016-09-19 10:08:26 +0300

    - added .gitignore file

--- src/main/java/StartXmlParser.java
+++ src/main/java/StartXmlParser.java
@@ -4,6 +4,7 @@ import service.MultiThreadService;
 public class StartXmlParser {
 
     public static void main(String[] args) {
+
         MultiThreadService x = new MultiThreadService();
         x.multiJob2();
     }
